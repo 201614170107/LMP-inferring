9ef4aeb07d0bee5224d47272f5971bb9d96bdbd5
Kedi Zheng <athoslove@vip.qq.com>
2018-05-10 11:48:13 +0800

    add B_estimate

--- main.m
+++ main.m
@@ -13,6 +13,9 @@ year = 2007;
 month = 12;
 day = 23;
 
+REF = find(mpc.bus(:,2) == 3);
+nb = size(mpc.bus, 1);    %% number of buses
+
 if exist('data/gen_data.csv','file') == 2
     gen_data = csvread('data/gen_data.csv');
 else
@@ -21,8 +24,15 @@ end
 
 [PI,PI_E,PI_C,PI_L,feasible,congestion,congest_idx] = get_lmp(mpc,gen_data);
 
-REF = find(mpc.bus(:,2) == 3);
 k = [0.01,0.01];
 B = B_Kekatos(PI_E,PI_C,REF, k);
 
-plot_mat(B,'jet');
+plot_mat(B,'parula');
+L = get_lap(B,REF);
+plot_mat(L,'jet');
+
+k = [0.01,0.01,0.01/(nb-1)];
+B = B_estimate(PI_E,PI_C,REF, k);
+plot_mat(B,'parula');
+L = get_lap(B,REF);
+plot_mat(L,'jet');
---
+++ src/B_estimate.m
@@ -0,0 +1,38 @@
+function B = B_estimate(PI_E,PI_C,REF,k,avg_degree,e)
+% function B = B_estimate(PI_E,PI_C,REF,k,avg_degree,e)
+if nargin < 6
+    e = 1e-3;
+    if nargin < 5
+        avg_degree = 0;
+    end
+end
+
+PI = PI_E+PI_C;
+kappa1 = k(1);
+kappa2 = k(2);
+kappa3 = k(3);
+nb = size(PI,1);
+PI = PI - repmat(PI(REF,:),nb,1);
+PI = PI([1:REF-1,REF+1:nb],:);
+IDX_congestion = sum(abs(PI_C),1)>0 ;
+PI = PI(:,IDX_congestion);
+
+B_solve = sdpvar(nb-1,nb-1);
+I = eye(nb-1);
+F = [B_solve >= 0, B_solve(:) <= I(:), B_solve*ones(nb-1,1) >= 0];
+P = I - ones(nb-1,nb-1);
+E = ones(1,nb-1);
+optimize(F,norm(B_solve*PI,1)+kappa1*trace(P*B_solve)-kappa2*logdet(B_solve)+kappa3*norm(E*B_solve,1),sdpsettings('solver','sdpt3'));
+
+BS = value(B_solve);
+
+if avg_degree>0
+    degree = mean(diag(BS));
+    B = BS * avg_degree/degree; 
+else 
+    B = BS;
+end
+B(abs(B)<e) = 0;
+
+
+end
